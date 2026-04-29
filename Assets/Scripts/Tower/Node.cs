f502c6ba4c30e1394105b26ab67c7a068a04ba57
Rizzz <alfrzxy@gmail.com>
2026-04-30 01:24:45 +0700

    Update logo palu di map

--- Assets/Scripts/Tower/Node.cs
+++ Assets/Scripts/Tower/Node.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+    [Header("Visual Link")]
+    // [PENTING]: Tarik objek PaluBangun ke slot ini di Inspector nanti!
+    [SerializeField] private GameObject hammerIcon; 
+
     private GameObject tower;
 
     void OnMouseDown()
@@ -17,10 +21,21 @@ public class Node : MonoBehaviour
 
         // Panggil BuildManager
         Debug.Log("<color=white>● [NODE] Minta BuildManager buat bangun...</color>");
+        
+        // Asumsi BuildManager.instance.BuildTowerOn balikin GameObject tower yang baru jadi
         tower = BuildManager.instance.BuildTowerOn(this);
         
         if(tower != null) {
             Debug.Log("<color=cyan>● [NODE] BuildManager lapor: BERHASIL!</color>");
+            
+            // --- LOGIKA MODULAR PENS: MATIIN PALU ---
+            if (hammerIcon != null) {
+                hammerIcon.SetActive(false); // Palu "pamit" undur diri
+                Debug.Log("<color=magenta>● [NODE] Visual Palu dimatikan!</color>");
+            }
+            
+            // Opsional: Matiin sprite kotak induknya biar bersih
+            GetComponent<SpriteRenderer>().enabled = false;
         } else {
             Debug.Log("<color=red>● [NODE] BuildManager lapor: GAGAL!</color>");
         }
